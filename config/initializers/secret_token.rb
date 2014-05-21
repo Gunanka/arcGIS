c0cfce06d3839bae90ee4b7c101778d81d2059bd
gunanka <gunanka@yobisoftware.com>
2014-05-21 23:35:08 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ArcserverDemo::Application.config.secret_token = '362be1ee8e288ef7aad07ae5d33a71ee405528357bb77af9f39c8680f522999933182a6b31a26dc02aab3004683897fea7c1d8f8413afad2b432c57b0258a088'
